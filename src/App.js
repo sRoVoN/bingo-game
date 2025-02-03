e4f71ac1c198c52e03233fdf90b5d2cf3fa8c3b0
DESKTOP-IBAEHR8 <soodeh.arvin@gmail.com>
2025-02-03 22:40:36 +0330

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import { useEffect, useState } from "react";
-import Confetti from "react-confetti";
+import { cardsArray } from "./utilis/cardsArray";
+import { matchedPatterns } from "./utilis/pattern";
 
-const cardsArray =[
-  {id:0, name:"Hello, hello?"},
-  {id:1, name:"I need to jump another call"},
-  {id:2, name:"Can everyone go on mute"},
-  {id:3, name:"Could you please get closer to the mic"},
-  {id:4, name:"(Loaud painful echo/ feedback)"},
-  {id:5, name:"Next slide please"},
-  {id:6, name:"Can we take this offLine?"},
-  {id:7, name:"is ___ on the call?"},
-  {id:8, name:"Could you share this slide afterwards?"},
-  {id:9, name:"Can somebody grant presenter right?"},
-  {id:10, name:"Can you email that to everyone?"},
-  {id:11, name:"Sorry I had problems loging in"},
-  {id:12, name:"(Animal noises in the background)"},
-  {id:13, name:"Sorry, I didn't find the conference ID"},
-  {id:14, name:"I was having connection issues"},
-  {id:15, name:"CONF call 😷 BINGO"},
-  {id:16, name:"Who just joined?"},
-  {id:17, name:"Sorry, something ___ with my calender"},
-  {id:18, name:"Do you see my screen?"},
-  {id:19, name:"Let's wait for ___!"},
-  {id:20, name: "You will send the ninutes?"},
-  {id:21, name: "Sorry I wwas on mute"},
-  {id:22, name:"Can you repeat please?"},
-  {id:23, name:"(Child noises in the background)"},
-  {id:24, name:"I'll have to get back to you"},
-]
+import Confetti from "react-confetti";
 
 
 function App() {
@@ -35,30 +10,15 @@ function App() {
   const [shaking, setShaking] = useState(null);
   const [clickedCells, setClickedCells] = useState([]);
   const [showConfetti, setShowConfetti] = useState(false);
-
   const fixedIndex = 15;
-  const matchedPatterns = [
-    [0,1,2,3,4],
-    [5,6,7,8,9],
-    [10, 11, 13,14],
-    [15, 16, 17, 18, 19],
-    [20, 21, 22, 23, 24],
-    [4, 8, 16, 20],
-    [0, 6, 18, 24],
-    [0, 5, 10, 15, 20],
-    [1, 6, 11, 16, 21],
-    [2, 7, 17, 22],
-    [3, 8, 13, 18, 23],
-    [4, 9, 14, 19, 24]
-  ] 
+
   const playSound = () => {
     const sound = new Audio('/sounds/acclaim.mp3'); // Path to your MP3 file
     sound.play();
   };
+
   const handleClick = (id, checkedArray, rowIndex, cellIndex) => {
     setShaking(id);
-
-
     setClickedCells((prevClickedCells) => {
       const updatedClickedCells = [
         ...prevClickedCells,
@@ -90,14 +50,6 @@ function App() {
     }
     return shuffledArray;
   };
-  const items = [];
-  for (let i = 0; i < cardsArray.length; i++) {
-    items.push(
-      <tr className="flex " key={cardsArray[i]}>
-        {cardsArray[i].name}
-      </tr>
-    );
-  }
   const shuffleArrayWithFixefCard = (array, fixedIndex) => {
     const fixedCard = array[fixedIndex];
     const cardsToShuffle = array.filter((_, index) => index !== fixedIndex);
---
+++ src/utilis/cardsArray.js
@@ -0,0 +1,29 @@
+// cardsData.js
+export const cardsArray = [
+    {id: 0, name: "Hello, hello?"},
+    {id: 1, name: "I need to jump another call"},
+    {id: 2, name: "Can everyone go on mute"},
+    {id: 3, name: "Could you please get closer to the mic"},
+    {id: 4, name: "(Loaud painful echo/ feedback)"},
+    {id: 5, name: "Next slide please"},
+    {id: 6, name: "Can we take this offLine?"},
+    {id: 7, name: "is ___ on the call?"},
+    {id: 8, name: "Could you share this slide afterwards?"},
+    {id: 9, name: "Can somebody grant presenter right?"},
+    {id: 10, name: "Can you email that to everyone?"},
+    {id: 11, name: "Sorry I had problems logging in"},
+    {id: 12, name: "(Animal noises in the background)"},
+    {id: 13, name: "Sorry, I didn't find the conference ID"},
+    {id: 14, name: "I was having connection issues"},
+    {id: 15, name: "CONF call 😷 BINGO"},
+    {id: 16, name: "Who just joined?"},
+    {id: 17, name: "Sorry, something ___ with my calendar"},
+    {id: 18, name: "Do you see my screen?"},
+    {id: 19, name: "Let's wait for ___!"},
+    {id: 20, name: "You will send the minutes?"},
+    {id: 21, name: "Sorry I was on mute"},
+    {id: 22, name: "Can you repeat please?"},
+    {id: 23, name: "(Child noises in the background)"},
+    {id: 24, name: "I'll have to get back to you"}
+  ];
+  
